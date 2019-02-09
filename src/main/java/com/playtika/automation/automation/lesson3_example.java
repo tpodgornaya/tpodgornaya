55f767891ca5cafd2dcff811b44676cc251402de
PLAYTIKA\tpodgornaya <tpodgornaya@playtika.com>
2019-02-09 19:48:41 +0200

    example with candies

--- src/main/java/com/playtika/automation/automation/lesson3_example.java
+++ src/main/java/com/playtika/automation/automation/lesson3_example.java
@@ -10,11 +10,14 @@ public class lesson3_example {
         float km = console.nextFloat();
         System.out.println("Введите время в минутах и нажмите Enter: ");
         float min = console.nextFloat();
-        float m = km * 1000;
-        float sec = min * 60;
-        float result = m / sec;
-        System.out.printf("Ваша сорость равна = %f ", result);
+
+        System.out.printf("Ваша сорость равна = %f ", calculateSpeed(km, min));
     }
 
+    public static float calculateSpeed(float distance, float time) {
+        float  speed = ((distance * 100)/(time * 60));
+        return speed;
+
+    }
 
 }
