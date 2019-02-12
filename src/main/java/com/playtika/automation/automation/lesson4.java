f9618b79160cbcdee96b1f27c53657b7f2abb06e
PLAYTIKA\tpodgornaya <tpodgornaya@playtika.com>
2019-02-12 16:42:26 +0200

    example with candies

---
+++ src/main/java/com/playtika/automation/automation/lesson4.java
@@ -0,0 +1,12 @@
+package com.playtika.automation.automation;
+
+public class lesson4 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        if (a == 10) {
+            System.out.println("Hello 1");
+        }
+        System.out.println("Hello 2");
+
+}
