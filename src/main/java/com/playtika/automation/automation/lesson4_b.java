30b8f71376ae62acc3b395404929a832f8cf79e0
PLAYTIKA\tpodgornaya <tpodgornaya@playtika.com>
2019-02-13 17:31:49 +0200

    example with candies

---
+++ src/main/java/com/playtika/automation/automation/lesson4_b.java
@@ -0,0 +1,11 @@
+package com.playtika.automation.automation;
+
+public class lesson4_b {
+
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+
+        System.out.println(str1 == str2);
+    }
+}
