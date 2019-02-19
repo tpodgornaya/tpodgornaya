8fc30797958d08218e57dbc96b3d97e945d82a3d
PLAYTIKA\tpodgornaya <tpodgornaya@playtika.com>
2019-02-19 18:57:14 +0200

    example with candies

--- src/main/java/com/playtika/automation/automation/HomeWork2_1.java
+++ src/main/java/com/playtika/automation/automation/HomeWork2_1.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class HomeWork2_1 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.println("Введите ширину: ");
-        int width = console.nextInt();
+        System.out.println("Введите  ширину ячейки: ");
+        int inputBlockSize = console.nextInt();
+        System.out.println(" * ");
+
+        // for (int i = 0; i < )
+
+        /*int width = console.nextInt();
         if (width < 3) {
             width = 3;
         }
         if (width > 9) {
             width = 9;
         }
-        int height = width / 3;
+        int height = width / 3;*/
     }
 }
