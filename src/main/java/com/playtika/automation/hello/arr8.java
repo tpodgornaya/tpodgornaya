60fe0fe5f603441c833abb70b9dff744387b6712
Oleksii Porokhnia <a.porohnya@gmail.com>
2019-02-20 22:37:02 +0200

    g

---
+++ src/main/java/com/playtika/automation/hello/arr8.java
@@ -0,0 +1,24 @@
+package com.playtika.automation.hello;
+
+public class arr8 {
+
+    public static void main(String[] args) {
+
+
+
+
+
+        for (int i = 0; i < 4; i++) {
+            //System.out.println(upperCase[i]);
+            for (int k = 0; k < 3; k++) {
+                System.out.print("* ");
+
+                //System.out.println("----" + lowerCase[k]);
+            }
+            System.out.println("*");
+
+        }
+
+        //System.out.println(maxNumber);
+    }
+}
