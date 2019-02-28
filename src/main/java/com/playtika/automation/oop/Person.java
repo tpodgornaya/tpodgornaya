1990c79cb09dd2c6c2c586f8d18fdd4c7eb8a7df
Oleksii Porokhnia <a.porohnya@gmail.com>
2019-02-28 22:05:57 +0200

    oop packange

---
+++ src/main/java/com/playtika/automation/oop/Person.java
@@ -0,0 +1,11 @@
+package com.playtika.automation.oop;
+
+public class Person extends Human {
+
+    public String mood;
+
+    public void getMyMood() {
+        System.out.printf("My mood is %s", mood);
+    }
+
+}
